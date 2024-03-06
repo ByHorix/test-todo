2af419911354f2b4b64479ac6221de0902ac5b41
YegorBaryshnykov <yb@if-else.cloud>
2024-03-06 15:03:58 +0200

    add adaptive to header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,15 +1,16 @@
 import React from "react"
-import {Button, CardHeader, CardTitle} from "reactstrap"
+import {Badge, Button, CardHeader, CardTitle} from "reactstrap"
 import {useAppDispatch, useAppSelector} from "../../utilities/hooks/redux"
 import {switchAddingNew} from "./store"
+import "./styles.css"
 
 export const Header = (): React.ReactElement => {
   const dispatch = useAppDispatch()
   const {isAddingNew} = useAppSelector((state) => state.layout)
 
   return (
-    <CardHeader className='d-flex justify-content-between'>
-      <CardTitle tag='h2'>"Operation: Get Stuff <span className='text-bg-success p-1 rounded-2'>Done</span>"</CardTitle>
+    <CardHeader className='d-flex justify-content-between card-header align-items-baseline flex-wrap'>
+      <CardTitle tag='h2'>"Operation: Get Stuff <span className='text-bg-success rounded-2 custom-badge'>Done</span>"</CardTitle>
       <Button
         color='primary'
         outline={true}
